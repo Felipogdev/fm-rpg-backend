4a3a97272de707e15276ab3ac536ecc112c97e66
installer <installer@gmail.com>
2025-06-25 01:28:13 +0100

    Feat: cors now allowing patch request

--- src/main/groovy/com/fmrpg/fmbackend/controllers/CharacterSkillController.java
+++ src/main/groovy/com/fmrpg/fmbackend/controllers/CharacterSkillController.java
@@ -3,12 +3,9 @@ package com.fmrpg.fmbackend.controllers;
 import com.fmrpg.fmbackend.dtos.CharacterSkillDto;
 import com.fmrpg.fmbackend.dtos.characterdtos.CharacterResponseDto;
 import com.fmrpg.fmbackend.entities.CharacterEntity;
-import com.fmrpg.fmbackend.entities.CharacterSkill;
 import com.fmrpg.fmbackend.mappers.CharacterResponseMapper;
 import com.fmrpg.fmbackend.repositories.CharacterRepository;
-import com.fmrpg.fmbackend.repositories.CharacterStatusRepository;
 import com.fmrpg.fmbackend.services.CharacterSkillsService;
-import com.fmrpg.fmbackend.services.CharacterStatusService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,18 +15,15 @@ import java.util.UUID;
 @RequestMapping("api/characters/skills")
 public class CharacterSkillController {
 
-    private final CharacterStatusRepository characterStatusRepository;
     private final CharacterRepository characterRepository;
     private final CharacterSkillsService characterSkillsService;
     private final CharacterResponseMapper characterResponseMapper;
 
     public CharacterSkillController(
-            CharacterStatusRepository characterStatusRepository,
             CharacterRepository characterRepository,
             CharacterSkillsService characterSkillsService,
             CharacterResponseMapper characterResponseMapper
     ) {
-        this.characterStatusRepository = characterStatusRepository;
         this.characterRepository = characterRepository;
         this.characterSkillsService = characterSkillsService;
         this.characterResponseMapper = characterResponseMapper;
@@ -44,8 +38,6 @@ public class CharacterSkillController {
         CharacterEntity character = characterRepository.findByPublicId(id);
 
        return ResponseEntity.ok(characterResponseMapper.toDto(characterSkillsService.updateSkill(dto, character)));
-
-
     }
 
 
