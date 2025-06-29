95e0bb419b16450a6f256fc4ba33ae63d5b78dec
installer <installer@gmail.com>
2025-06-29 02:37:08 +0100

    Feat: characteritem class

--- src/main/groovy/com/fmrpg/fmbackend/entities/CharacterItem.java
+++ src/main/groovy/com/fmrpg/fmbackend/entities/CharacterItem.java
@@ -1,21 +1,37 @@
 package com.fmrpg.fmbackend.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fmrpg.fmbackend.entities.characterpkg.CharacterEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "character_item")
 @Getter
 @Setter
-public class CharacterItem extends ItemAbstract {
+@NoArgsConstructor
+@AllArgsConstructor
+public class CharacterItem {
 
     @Id
-    Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String name;
 
+    private Integer quantity;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id", nullable = false)
+    private ItemAbstract item;
+
+    @ManyToOne
+    @JoinColumn(name = "character_id", nullable = false)
+    private CharacterEntity character;
 
 }
---
+++ src/main/groovy/com/fmrpg/fmbackend/repositories/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.fmrpg.fmbackend.repositories;
+
+import com.fmrpg.fmbackend.entities.ItemAbstract;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<ItemAbstract, Long> {
+}
