a58af5cacd96573b0c6a124ffadeae4d6a227cee
installer <installer@gmail.com>
2025-07-18 00:39:45 +0100

    fixed character response with charactermulticlass infinite json

--- src/main/groovy/com/fmrpg/fmbackend/dtos/characterdtos/CharacterMulticlassResponseDto.java
+++ src/main/groovy/com/fmrpg/fmbackend/dtos/characterdtos/CharacterMulticlassResponseDto.java
@@ -1,10 +1,9 @@
 package com.fmrpg.fmbackend.dtos.characterdtos;
 
 import com.fmrpg.fmbackend.entities.characterpkg.CharacterClass;
-import com.fmrpg.fmbackend.entities.characterpkg.CharacterEntity;
 
 public record CharacterMulticlassResponseDto(
-        CharacterClass characterClass,
+        CharacterClassDto characterClass,
         Integer level
 ) {
 }
